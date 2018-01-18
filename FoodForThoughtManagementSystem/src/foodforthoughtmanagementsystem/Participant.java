2db172270a8fba8fd2aa930b0581c8df86a549bc
340745868 <340745868@109R030573XCQ22.EDU.YRDSB.NET>
2018-01-18 12:01:40 -0500

    Merge origin/master
    Conflicts:
    	FoodForThoughtManagementSystem/src/foodforthoughtmanagementsystem/Members.java

--- FoodForThoughtManagementSystem/src/foodforthoughtmanagementsystem/Participant.java
+++ FoodForThoughtManagementSystem/src/foodforthoughtmanagementsystem/Participant.java
@@ -5,7 +5,16 @@
  */
 package foodforthoughtmanagementsystem;
 
+import foodforthoughtmanagementsystem.panels.Panel01_LoginScreen;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,13 +22,23 @@ import java.io.File;
  */
 public class Participant{ //extends Hours{
 
-    public Participant(String firstName,String lastName, int studentNumber,int totalHours, char status) {
+    public Participant(String firstName,String lastName, int studentNumber,double totalHours, char status) {
        String fn= firstName;
        String ln= lastName;
        int sn= studentNumber;
-       int th= totalHours;
+       double th= totalHours;
        char s= status;
        
     }    
+    public void updateHours(double moreHours){
+        String login= Panel01_LoginScreen.loginNumber;
+        File user= new File(login);
+        try {
+            Scanner s= new Scanner(user);
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Participant.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
